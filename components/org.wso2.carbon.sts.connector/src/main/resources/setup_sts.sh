a38b13196a15afde7429d9434b1fed4047c13579
deshankoswatte <dehami.deshan@gmail.com>
2020-05-04 17:01:36 +0530

    Migrate the zip file making to a new component

---
+++ components/org.wso2.carbon.sts.connector/src/main/resources/setup_sts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mv ./dropins/* ../repository/components/dropins/
+mv ./deployment-client-modules/* ../repository/deployment/client/modules/
+mv ./deployment-server-webapps/mexut.war ../repository/deployment/server/webapps/
+mkdir ../repository/deployment/server/webapps/mex/
+mv ./deployment-server-webapps/metadata.xml ../repository/deployment/server/webapps/mex/
+mv ./other-files/sts_response.html ../repository/resources/identity/pages/
