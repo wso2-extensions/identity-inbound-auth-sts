443cec021d696972c113c8723e453039d9ba2369
deshankoswatte <dehami.deshan@gmail.com>
2020-06-26 20:00:44 +0530

    Remove mex components from the connector and fix them to pack by default

--- components/org.wso2.carbon.sts.connector/src/main/resources/setup_sts.sh
+++ components/org.wso2.carbon.sts.connector/src/main/resources/setup_sts.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 mv ./dropins/* ../repository/components/dropins/
 mv ./deployment-client-modules/* ../repository/deployment/client/modules/
-mv ./deployment-server-webapps/mexut.war ../repository/deployment/server/webapps/
-mkdir ../repository/deployment/server/webapps/mex/
-mv ./deployment-server-webapps/metadata.xml ../repository/deployment/server/webapps/mex/
