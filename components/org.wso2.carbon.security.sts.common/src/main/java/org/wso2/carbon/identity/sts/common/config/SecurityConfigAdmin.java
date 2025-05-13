4ad2b368b36867bd577f0b332fe44865bb6515b8
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2025-05-13 18:14:53 +0530

    Add support for configure custom keystores for ws-trust and ws-fed

--- components/org.wso2.carbon.security.sts.common/src/main/java/org/wso2/carbon/identity/sts/common/config/SecurityConfigAdmin.java
+++ components/org.wso2.carbon.security.sts.common/src/main/java/org/wso2/carbon/identity/sts/common/config/SecurityConfigAdmin.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020-2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.sts.common.config;
@@ -57,6 +59,11 @@ import org.wso2.carbon.core.util.CryptoException;
 import org.wso2.carbon.core.util.CryptoUtil;
 import org.wso2.carbon.core.util.KeyStoreManager;
 import org.wso2.carbon.core.util.KeyStoreUtil;
+import org.wso2.carbon.identity.base.IdentityRuntimeException;
+import org.wso2.carbon.identity.core.IdentityKeyStoreResolver;
+import org.wso2.carbon.identity.core.util.IdentityKeyStoreResolverConstants;
+import org.wso2.carbon.identity.core.util.IdentityKeyStoreResolverException;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.sts.common.SecurityConfigParams;
 import org.wso2.carbon.identity.sts.common.SecurityScenario;
@@ -560,6 +567,21 @@ public class SecurityConfigAdmin {
         }
         // First disable security and remove all applied policies before applying a new policy
         this.disableSecurityOnService(serviceName);
+
+        // Override keystore settings with custom WS-Trust specific keystore configuration
+        // if available for the tenant.
+        try {
+            String tenantDomain = IdentityTenantUtil.getTenantDomain(((UserRegistry) registry).getTenantId());
+            String keyStoreName = IdentityKeyStoreResolver.getInstance()
+                    .getKeyStoreName(tenantDomain, IdentityKeyStoreResolverConstants.InboundProtocol.WS_TRUST);
+            if (KeyStoreUtil.isCustomKeyStore(keyStoreName)) {
+                privateStore = keyStoreName;
+                trustedStores = new String[] {keyStoreName};
+            }
+        } catch (IdentityKeyStoreResolverException | IdentityRuntimeException e) {
+            String msg = "Error while retrieving the keystore configurations for the tenant";
+            throw new SecurityConfigException(msg, e);
+        }
 
         OMElement policyElement = loadPolicyAsXML(scenarioId, policyPath);
         SecurityScenario scenario = SecurityScenarioDatabase.get(scenarioId);
